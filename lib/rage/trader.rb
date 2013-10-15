811a8cbdf6d0b708b0fa76884df5f57a256ade92
Bryan Brandau <agent462@gmail.com>
2013-10-14 23:16:09 -0500

    trading logic for buy/sell

--- lib/rage/trader.rb
+++ lib/rage/trader.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'pp'
 
 module Rage
   class Trader
@@ -9,35 +10,62 @@ module Rage
     end
 
     def buy
-      buy = @mtgox.can_buy
+      can_buy = @mtgox.can_buy
+      can_buy > Config.max_buy.to_f ? amount = Config.max_buy.to_f : amount = can_buy
+      if amount < 0.01
+        @logger.info('The number of btc you can buy is below the minimum 0.01.')
+        return
+      end
       price = @mtgox.current_price
-      total = buy * price
-      @mtgox.buy(buy, price)
-      @logger.info("Attempting to buy #{buy} bitcoins at $#{price} for a total of $#{total}.")
-      #save_trade(purchase)
+      total = amount.to_f * price.to_f
+      @logger.info("Attempting to buy #{amount} bitcoins at $#{price} for a total of $#{total}.")
+      bid = @mtgox.buy(amount)
+      while @mtgox.get_buys.count > 0
+        sleep(5)
+      end
+      order = @mtgox.order_result('bid', bid)
+      @logger.info("Bought #{order.total_amount.to_f} btc for $#{order.total_spent.to_f} with an average cost of $#{order.avg_cost.to_f}.")
+      save_trade({
+          :id => order.id,
+          :total_amount => order.total_amount,
+          :avg_cost => order.avg_cost,
+          :total => order.total_spent
+        }, 'bid')
     end
 
     def sell
       btc = @mtgox.get_btc_balance
-      @mtgox.sell(btc)
-      @logger.info("Sold #{btc} bitcoins.")
-      #save_trade(purchase)
+      btc > Config.max_sell.to_f ? amount = Config.max_sell.to_f : amount = btc
+      ask = @mtgox.sell(amount)
+      @logger.info("Attempting to sell #{amount} bitcoins.")
+      while @mtgox.get_sells.count > 0
+        sleep(5)
+      end
+      order = @mtgox.order_result('ask', ask)
+      @logger.info("Sold #{order.total_amount.to_f} btc for $#{order.total_spent.to_f} with an average price of $#{order.avg_cost.to_f}.")
+      save_trade({
+          :id => order.id,
+          :total_amount => order.total_amount,
+          :avg_cost => order.avg_cost,
+          :total => order.total_spent
+        }, 'ask')
     end
 
-    def hold?
-    end
-
-    def save_trade(purchase)
+    def save_trade(order, type)
       trade = {
-        :timestamp => Time.now.to_i,
-        :type => purchase[:type],
-        :amount => purchase[:btc],
-        :price => purchase[:price],
-        :total => purchase[:btc] * purchase[:price]
+        :id => order[:id],
+        :type => type,
+        :amount => order[:total_amount],
+        :price => order[:avg_cost],
+        :total => order[:total_spent]
       }
       redis = Redis.new(:host => Config.redis_host, :port => Config.redis_port)
-      redis.sadd('trades', trade.to_json)
+      redis.sadd('trades', trade[:id])
+      redis.set("trade:#{trade[:id]}", trade.to_json)
       @logger.info("Trade has been saved.")
+    end
+
+    def calculate_profits
     end
 
   end
