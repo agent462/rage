5696cff5d1c031b9f70f7cbb78e8fcde603dc0bb
Bryan Brandau <agent462@gmail.com>
2013-10-17 00:41:44 -0500

    current rewrite of max formula

--- lib/rage/formulas/max.rb
+++ lib/rage/formulas/max.rb
@@ -1,6 +1,5 @@
 require 'net/http'
 require 'csv'
-require 'json'
 
 module Rage
   class Max
@@ -9,11 +8,16 @@ module Rage
       @logger = Rage.logger
     end
 
-    def schedule
-      %w[01:25 07:25 13:25 19:25]
+    def redis
+      @redis ||= Redis.new(:host => Config.redis_host, :port => Config.redis_port)
+    end
+
+    def brains
+      @brains ||= %w[ 1_hour_analysis_last 3_hour_analysis_last 6_hour_analysis_last 12_hour_analysis_last 24_hour_analysis_last ]
     end
 
     def fetch
+      @logger.info('Fetching data from Max.')
       uri = URI(Config.max_uri)
       CSV.parse(Net::HTTP.get(uri))
     rescue StandardError
@@ -26,38 +30,74 @@ module Rage
     #  Sell: sell btc if we are holding btc
     #  Hold: don't take any action
     #
-    def trade(data)
-      data.each do |d|
-        if d[0] == '1_hour_analysis_last'
-          redis = Redis.new(:host => Config.redis_host, :port => Config.redis_port)
-          old_value = redis.get('1_hour_max')
-          redis.set('1_hour_max', d[2])
-          if old_value
-            case d[2]
-            when "-1"
-              if old_value == '1' || old_value == '0'
-                return 'sell'
-              else
-                return 'hold'
-              end
-            when "0"
-              return 'hold'
-            when "1"
-              if old_value == '-1' || old_value == '0'
-                return 'buy'
-              else
-                return 'hold'
-              end
-            end
-          else
-            @logger.info('No previous data to go by.  Initial recommendation is to hold.')
-            return 'hold' # when redis doesn't have an old value
+    #  Redis Set max:3_hour_analysis_last id
+    #  Redis Hash max:3_hour_analysis_last:id
+    def collect
+      data = fetch
+      brains.each do |brain|
+        data.each do |d|
+          if d.include? brain
+            @logger.info("Saving #{d[0]} brain.")
+            time = Time.now.to_i
+            redis.zadd("max:#{d[0]}", time, "max:#{brain}:#{time}")
+            redis.hmset("max:#{brain}:#{time}", 'timestamp', time, 'fitness', d[1], 'signal', d[2], 'last_data', d[3])
           end
         end
       end
-        @logger.error('Insufficient data returned from Max.  You might want to check manually.'.color(:red))
-        return 'hold' # insufficient data
+      get_brains
+    end
+
+    def get_brains
+      brains.each do |brain|
+        values = get_values(brain)
+        response(values)
+      end
+    end
+
+    def get_brain(brain = Config.max_brain)
+      values = get_values(brain)
+      response(values)
+    end
+
+    def response(values)
+      if enough_data?(values)
+        if signal_change?(values)
+          puts recommendation(values[0][1], values[1][1])
+        else
+          puts 'hold'
+        end
+      else
+        @logger.error('Not enough data')
+      end
+    end
+
+    def get_values(brain)
+      redis.sort(
+                  "max:#{brain}",
+                  :by => 'nosort',
+                  :get => '#',
+                  :get => ['*->timestamp', '*->signal'],
+                  :order => 'desc',
+                  :limit => [0,2]
+                )
+    end
+
+    def recommendation(current, previous)
+      return 'buy' if current.to_i > previous.to_i && current != '0'
+      return 'sell' if current.to_i < previous.to_i && current != '0'
+      return 'hold'
+    end
+
+    def enough_data?(values)
+      max = values.max_by { |x| x}
+      min = values.min_by { |x| x}
+      (max[0].to_i - min[0].to_i) < 1200 ? true : false
+    end
+
+    def signal_change?(values)
+      values[0][1] == values[1][1] ? true : false
     end
 
   end
 end
+
