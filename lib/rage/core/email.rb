b8519a7c27687c0361aceafbc8f79825cd0d241b
Bryan Brandau <agent462@gmail.com>
2013-10-20 13:10:47 -0500

    email is static

--- lib/rage/core/email.rb
+++ lib/rage/core/email.rb
@@ -2,25 +2,27 @@ require 'mail'
 
 module Rage
   class Email
+    class << self
 
-    def send_email(message = {})
-      Mail.defaults do
-        delivery_method :smtp, {
-          :address => 'smtp.gmail.com',
-          :port => '587',
-          :user_name => Config.email,
-          :password => Config.email_password,
-          :authentication => :plain,
-          :enable_starttls_auto => true
-        }
+      def send_email(message = {})
+        Mail.defaults do
+          delivery_method :smtp, {
+            :address => 'smtp.gmail.com',
+            :port => '587',
+            :user_name => Config.email,
+            :password => Config.email_password,
+            :authentication => :plain,
+            :enable_starttls_auto => true
+          }
+        end
+        Mail.deliver do
+          from    Config.email
+          to      Config.email
+          subject "Rage Trader: Recommendation #{message[:advice]}"
+          body    message[:body]
+        end
       end
-      Mail.deliver do
-         from    Config.email
-         to      Config.email
-         subject "Rage Trader: Recommendation #{message[:advice]}"
-         body    message[:body]
-      end
+
     end
-
   end
 end
