849bc15ebff912c2efea704f204142101fe586f5
Bryan Brandau <agent462@gmail.com>
2013-10-20 13:14:46 -0500

    updates to decision

--- lib/rage/decision.rb
+++ lib/rage/decision.rb
@@ -1,27 +1,37 @@
 module Rage
   class Decision
 
-    def make(results)
+    def initialize
+      base = Base.new
+      @logger = base.logger
+    end
+
+    def make(advice)
       mtgox = MtGox.new
-      if results[:max] == "buy"
+      if advice == 'buy'
         if mtgox.has_btc?
-          @logger.info("We already own btc.  Not buying.")
-          trade = Trader.new
-          trade.buy
+          @logger.info('We already own btc.  Not buying.')
         else
-          @logger.info("I am buying.")
+          @logger.info('I am buying.')
+          # trade = Trader.new
+          # trade.buy
         end
-      elsif results[:max] == "sell"
+      elsif advice == 'sell'
         if mtgot.has_btc?
-          @logger.info("I am selling.")
-          trade = Trader.new
-          trade.sell
+          @logger.info('I am selling.')
+          # trade = Trader.new
+          # trade.sell
         else
           @logger.info("We don't own any btc to sell.  Not doing anything")
         end
       else
-        @logger.info("Suggestion is to hold.  We are not performing a trade at this time.")
+        @logger.info('The suggestion is to hold.  We are not performing a trade at this time.')
       end
+      email(advice)
+    end
+
+    def email(advice)
+      Email::send_email(:advice => advice, :message => 'todo')
     end
 
   end
