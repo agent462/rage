cdcae7447554cb50bd3b496aebaf1f18c44ed5ee
Bryan Brandau <agent462@gmail.com>
2013-10-20 13:14:01 -0500

    add email and remove eventmachine

--- lib/rage.rb
+++ lib/rage.rb
@@ -1,13 +1,14 @@
 require 'rage/base'
-require 'eventmachine'
 require 'rage/core/settings'
 require 'rage/core/log'
+require 'rage/core/email'
 require 'rage/scheduler'
 require 'rage/version'
 require 'rage/formulas/moving_average'
 require 'rage/formulas/max'
 require 'rage/aggregator'
 require 'rage/trader'
+require 'rage/decision'
 
 module Rage
 end
