ccfa642b0fe3fbc56be733c6b5865c165e9d72c2
Bryan Brandau <agent462@gmail.com>
2013-10-20 13:17:57 -0500

    settings updates

--- settings.example.rb
+++ settings.example.rb
@@ -3,10 +3,10 @@ mtgox_key     ''
 mtgox_secret  ''
 
 # Should the bot perform active trades
-trade         true
+trade          true
 max_buy       0.01 # max btc to buy
 max_sell      0.01 # max btc to sell
-commission    0.6 # your commission amount
+commission    0.6
 
 # Host and port for redis
 redis_host    '127.0.0.1'
@@ -14,3 +14,8 @@ redis_port    '6379'
 
 # URI for Max formula
 max_uri       ''
+max_brain     '1_hour_analysis_last'
+
+# email uses gmail
+email ''
+email_password  ''
