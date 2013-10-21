ae427a86a393492de97e8f696b8054f00b1099a7
Bryan Brandau <agent462@gmail.com>
2013-10-20 19:33:25 -0500

    update scheduler time

--- lib/rage/scheduler.rb
+++ lib/rage/scheduler.rb
@@ -12,29 +12,29 @@ module Rage
         max = Max.new
         agg = Aggregator.new
 
-        scheduler.every '15m', :first_at => Time.now + 10 do
+        scheduler.every '15m', :first_at => Time.now + 5 do
           account = mtgox.get_balance
           logger.info('Account information'.color(:green))
           logger.info("USD: $#{account["USD"]}".color(:green))
           logger.info("BTC: #{account["BTC"]}".color(:green))
         end
 
-        scheduler.every '5m', :first_at => Time.now + 10 do
+        scheduler.every '5m', :first_at => Time.now + 5 do
           agg.get_current_price
         end
 
-        # scheduler.every '10m', :first_at => Time.now + 10 do
+        # scheduler.every '10m', :first_at => Time.now + 5 do
         #   agg = Aggregator.new
         #   agg.prime
         # end
 
-        scheduler.every '15m', :first_at => Time.now + 10 do
+        scheduler.every '15m', :first_at => Time.now + 5 do
           max.collect
           advice = max.get_brain
           logger.info("The recommendation from Max is to #{advice}".color(:cyan))
           dec = Decision.new
           dec.make(advice)
-          max.get_brains
+          max.display_brains
         end
 
         scheduler.join
