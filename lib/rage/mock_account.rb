dfd927d2e05e212a3abba5eb46297d5fe144b218
Bryan Brandau <agent462@gmail.com>
2013-10-26 19:22:15 -0500

    additional mock trading logic

---
+++ lib/rage/mock_account.rb
@@ -0,0 +1,34 @@
+module Rage
+  class MockAccount
+
+    def balance
+      if Rage.redis.exists('mock:account:balance')
+        Rage.redis.get('mock:account:balance').to_f
+      else
+        save_balance(Config.mock_dollars || 1000)
+        Config.mock_dollars.to_f
+      end
+    end
+
+    def btc
+      Rage.redis.exists('mock:account:btc') ? Rage.redis.get('mock:account:btc') : 0
+    end
+
+    def save_balance(balance)
+      Rage.redis.set('mock:account:balance', balance)
+    end
+
+    def save_btc(btc)
+      Rage.redis.set('mock:account:btc', btc)
+    end
+
+    def can_buy(current_price)
+      Integer(((balance - commission) / current_price.to_f) * 100000000) / Float(100000000)
+    end
+
+    def commission
+      (balance * Config.commission) / 100
+    end
+
+  end
+end
--- lib/rage/trader.rb
+++ lib/rage/trader.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'rage/mock_account'
 
 module Rage
   class Trader
@@ -6,57 +7,71 @@ module Rage
 
     def initialize
       @mtgox = MtGox.new
+      @account = MockAccount.new if is_mock?
+      @price = @mtgox.current_price
     end
 
     def buy
-      amount = 0.2 # buy_amount
+      amount = buy_amount
       return logger.info('The number of btc you can buy is below the minimum 0.01.') if amount < 0.01
-      price = @mtgox.current_price
-      total = amount.to_f * price.to_f
-      logger.info("Attempting to buy #{amount} bitcoins at $#{price} for a total of $#{total}.")
-      if Config.trade
+      total = total(@price, amount)
+      logger.info("Attempting to buy #{amount} bitcoins at $#{@price} for a total of $#{total}.")
+      if is_mock?
+        logger.info("Making a paper buy trade.")
+        save_trade(mock_trade(Time.now.to_i, 'buy', amount, @price, total))
+        @account.save_btc(amount)
+        @account.save_balance(0)
+      else
         bid = @mtgox.buy!(amount, :market)
         sleep(5) while @mtgox.buys.count > 0
         order = @mtgox.order_result('bid', bid)
         save_trade(mock_trade(order.id, 'buy', order.total_amount, order.avg_cost, order.total_spent))
-      else
-        logger.info("Making a paper buy trade.")
-        save_trade(mock_trade(Time.now.to_i, 'buy', amount, price, total))
       end
     end
 
+    def is_mock?
+      @is_mock ||= !Config.trade
+    end
+
+    def total(price, amount)
+      Integer(((amount.to_f * price.to_f)) * 100000) / Float(100000)
+    end
+
     def buy_amount
-      can_buy = @mtgox.can_buy
-      (Config.max_buy.nil? || can_buy <= Config.max_buy.to_f) ? can_buy : Config.max_buy.to_f
+      can_buy = is_mock? ? @account.can_buy(@price) : @mtgox.can_buy
+      (Config.max_buy.nil? || can_buy <= Config.max_buy.to_f) ? can_buy.to_f : Config.max_buy.to_f
     end
 
     def sell
       amount = sell_amount
+      return logger.info('The number of bitcon you have to sell is below the minimum .01') if amount < 0.01
       logger.info("Attempting to sell #{amount} bitcoins.")
-      if Config.trade
+      if is_mock?
+        logger.info("Making a paper sell trade.")
+        total = total(@price, amount)
+        save_trade(mock_trade(Time.now.to_i, 'sell', amount, @price, total))
+        @account.save_btc(0)
+        @account.save_balance(total)
+      else
         ask = @mtgox.sell!(amount, :market)
         sleep(5) while @mtgox.sells.count > 0
         order = @mtgox.order_result('ask', ask)
         save_trade(mock_trade(order.id, order.total_amount, order.avg_cost, order.total_spent))
-      else
-        logger.info("Making a paper sell trade.")
-        price = @mtgox.current_price
-        save_trade(mock_trade(Time.now.to_i, 'sell', amount, price, (price * amount)))
       end
     end
 
     def sell_amount
-      btc = @mtgox.get_btc_balance
-      (Config.max_sell.nil? || btc <= Config.max_sell.to_f) ? btc : Config.max_sell.to_f
+      can_sell = is_mock? ? @account.btc : @mtgox.get_btc_balance
+      (Config.max_sell.nil? || can_sell <= Config.max_sell.to_f) ? can_sell.to_f : Config.max_sell.to_f
     end
 
     def save_trade(trade)
-      if Config.trade
-        Rage.redis.sadd('trades', trade[:id])
-        Rage.redis.set("trade:#{trade[:id]}", trade.to_json)
-      else
+      if is_mock?
         Rage.redis.sadd('trades:mock', trade[:id])
         Rage.redis.set("trade:mock:#{trade[:id]}", trade.to_json)
+      else
+        Rage.redis.sadd('trades', trade[:id])
+        Rage.redis.set("trade:#{trade[:id]}", trade.to_json)
       end
       logger.debug('Trade has been saved.')
     end
