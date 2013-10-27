6c7fc8111a0dc161c4b6e420722d8db1a55120f6
Bryan Brandau <agent462@gmail.com>
2013-10-27 14:20:44 -0500

    change logger output format

--- lib/rage/core/log.rb
+++ lib/rage/core/log.rb
@@ -12,15 +12,18 @@ module Rage
     # Sets up logging device
     #
     def self.logger
-      unless @logger
-        dir = File.join(Dir.home, '.rage/log/')
-        file = File.join(dir, 'rage.log')
-        FileUtils.mkdir_p(dir) unless File.directory?(dir)
-        log_file = File.open(file, 'a+')
-        STDOUT.sync = true
-        log_file.sync = true
+      return @logger if @logger
+      dir = File.join(Dir.home, '.rage/log/')
+      file = File.join(dir, 'rage.log')
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+      log_file = File.open(file, 'a+')
+      STDOUT.sync = true
+      log_file.sync = true
+      @logger = Logger.new(MultiIO.new(STDOUT, log_file))
+      @logger.formatter = proc do |severity, datetime, progname, msg|
+        "[#{datetime}] #{severity} -- : #{msg}\n"
       end
-      @logger ||= Logger.new(MultiIO.new(STDOUT, log_file))
+      @logger
     end
 
     def self.set_level(level)
